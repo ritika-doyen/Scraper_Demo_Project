7939940f4300f99fc60429f398c2025ce79642a8
Ritika <sharitika9116@gmail.com>
2025-08-08 23:13:16 +0530

    Updated

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-echo "Installing Playwright browser dependencies..."
-pip install -r requirements.txt
-playwright install chromium
 
-echo "Installing Playwright browsers..."
+echo "▶ Installing Python dependencies..."
+pip install -r requirements.txt
+
+echo "▶ Installing Playwright browsers..."
 playwright install --with-deps
+
 
